5b31b249e7f0d32385dce451472223e3b1bfc7d0
马玥 <mayue@eastmoney.com>
2021-09-13 14:40:03 +0800

    feat:html解析，dom树

--- week02/parser.js
+++ week02/parser.js
@@ -1,8 +1,50 @@
-let currentToken = null
-let currentAttribute = null
+let currentToken = null // 当前标签
+let currentAttribute = null // 当前属性
+let currentTextNode = null  // 当前的文本节点
+
+let stack = [{type: 'document', children: {}}] // 结点树
 
 function emit(token) {
-  // if(token.type !== 'text)
+  let top = stack[stack.length - 1]
+
+  if(token.type === 'startTag') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: []
+    }
+    element.tagName = token.tabName
+    for(let p in token) {
+      if(p !== 'type' && p !== 'tagName') {
+        element.attributes.push({
+          name: p,
+          value: token[p]
+        })
+      }
+    }
+    top.children.push(element)
+    element.parent = top
+    if(!token.isSelfClosing) {
+      stack.push(element)
+    }
+    currentTextNode = null
+  } else if(token.type === 'endTag') {
+    if(top.tagName !== token.tagName) {
+      throw new Error("Tag start end doesn't match!")
+    } else {
+      stack.pop()
+    }
+    currentTextNode = null
+  } else if(token.type === 'text') {
+    if(currentTextNode === null) {
+      currentTextNode = {
+        type: 'text',
+        content: ''
+      }
+      top.children.push(currentTextNode)
+    }
+    currentTextNode.content += token.content
+  }
   console.log(token)
 }
 
@@ -104,6 +146,93 @@ function attributeName(c) {
   }
 }
 
+// 属性值前
+function beforeAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    return beforeAttributeValue
+  } else if(c === '\"') {
+    return doubleQuotedAttributeValue
+  } else if(c === '\'') {
+    return singleQuotedAttributeValue
+  } else if(c === '>') {
+
+  } else {
+    return UnquotedAttributeValue(c)
+  }
+}
+
+// 引号后
+function afterQuotedAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName
+  } else if(c === '/') {
+    return selfClosingStartTag
+  } else if(c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data 
+  } else if(c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  }
+}
+
+// 双引号
+function doubleQuotedAttributeValue(c) {
+  if(c === '\"') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if(c === '\u0000') {
+
+  } else if(c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  }
+}
+
+// 单引号
+function singleQuotedAttributeValue(c) {
+  if(c === '\"') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if(c === '\u0000') {
+
+  } else if(c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return singleQuotedAttributeValue
+  }
+}
+
+// 无引号
+function UnquotedAttributeValue(c) {
+  if(c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return beforeAttributeName
+  } else if(c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return selfClosingStartTag
+  } else if(c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if(c === '\u0000') {
+
+  } else if(c === '\"' || c === '\'' || c === '<' || c === "'") {
+
+  } else if(c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return UnquotedAttributeValue
+  }
+}
+
 // 自封闭标签
 function selfClosingStartTag(c) {
   if(c === '>') {
@@ -124,4 +253,6 @@ module.exports.parseHTML = function parseHTML(html) {
     state = state(c)
   }
   state = state(EOF)
+  console.log(state)
+  return stack[0]
 }
